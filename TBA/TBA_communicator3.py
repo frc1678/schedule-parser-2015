fba341c26dead2a369fb0fbb71d7b73a76b9527b
Nilocunger <mastakata3@yahoo.com>
2015-04-10 02:31:53 -0700

    Added change packet creation for schedule. Added change packets to .gitignore

---
+++ TBA/TBA_communicator3.py
@@ -0,0 +1,65 @@
+from urllib.request import Request, urlopen
+
+# Created by Colin Unger 
+# Year: 2015
+
+basicURL = "http://www.thebluealliance.com/api/v2/"
+
+headers = {"X-TBA-App-Id" : "cdu:TBA_communicator:0"} # set to "<your initials>:TBA_communicator:0"
+
+def setHeadersForApp(yourName, appName, versionNumber):
+	headers = {"X-TBA-App-Id" : str(yourName) + str(appName) + str(versionNumber)}
+
+def makeRequest(url):
+	#print url
+	request = Request(url, headers = headers)
+	return urlopen(request).read().decode("utf-8")
+
+def getTeamListRequestKey(pageNumber):
+	return "teams/{pageNumber}".format(pageNumber = pageNumber)
+
+def makeTeamListRequest(pageNumber):
+	return makeRequest(basicURL + getTeamListRequestKey(pageNumber))
+
+def getTeamRequestKey(teamNumber):
+	return "team/frc{teamNumber}".format(teamNumber = teamNumber)
+
+def makeTeamRequest(teamNumber):
+	return makeRequest(basicURL + getTeamRequestKey(teamNumber))
+
+def getTeamEventsRequestKey(teamNumber, year):
+	return "team/frc{teamNumber}/{year}/events".format(teamNumber = teamNumber, year = year)
+
+def makeTeamEventsRequest(teamNumber, year):
+	return makeRequest(basicURL + getTeamEventsRequestKey(teamNumber, year))
+
+def getTeamEventAwardsRequestKey(teamNumber, competition, year):
+	return "/api/v2/team/frc{teamNumber}/event/{year}{competition}/awards".format(teamNumber = teamNumber, year = year, competition = competition)
+
+def makeTeamEventAwardsRequest(teamNumber, year):
+	return #finish this later
+
+def getEventRequestKey(competition, year):
+	return "event/{year}{competition}".format(competition = competition, year = year)
+
+def makeEventRequest(competition, year):
+	return makeRequest(basicURL + getEventRequestKey(competition, year))
+
+def getEventMatchesRequestKey(competition, year):
+	return "event/{year}{competition}/matches".format(competition = competition, year = year)
+
+def makeEventMatchesRequest(competition, year):
+	return makeRequest(basicURL + getEventMatchesRequestKey(competition, year))
+
+def getEventTeamsRequestKey(competition, year):
+	return "event/{year}{competition}/teams".format(competition = competition, year = year)
+
+def makeEventTeamsRequest(competition, year):
+	return makeRequest(basicURL + getEventTeamsRequestKey(competition, year))
+
+def getEventStatsRequestKey(competition, year):
+	return "event/{year}{competition}/stats".format(competition = competition, year = year)
+
+def makeEventStatsRequest(competition, year):
+	return makeRequest(basicURL + getEventStatsRequestKey(competition, year))
+
---
+++ TBA/schedule_TBA_change_packets.py
@@ -0,0 +1,67 @@
+import TBA_communicator3 as tba
+import json
+import os
+import shutil as sh
+
+# Created by Colin Unger 
+# Year: 2015
+
+keys = ['uploadedData.maxFieldToteHeight', 'uploadedData.stackingAbility', 'match.officialRedScore', 'match.officialBlueScore']
+
+competition = input("Competition: ")
+year = int(input("Year: "))
+
+print('\n')
+
+print('Getting event info for competition ' + competition)
+event = json.loads(tba.makeEventRequest(competition, year))
+print('Got event info for competition ' + competition)
+
+eventName = event['name']
+compCode = event['key']
+
+teamsJSON = json.loads(tba.makeEventTeamsRequest(competition, year))
+teamNames = {}
+for team in teamsJSON:
+	teamNames[team['team_number']] = team['nickname']
+
+print('Getting teams at event ' + competition)
+matchesJSON = json.loads(tba.makeEventMatchesRequest(competition, year))
+print('Got teams at event ' + competition)
+
+if os.path.isdir('Change Packets'):
+	print('Deleting directory "Change Packets"')
+	sh.rmtree('Change Packets')
+	print('Deleted directory "Change Packets"')
+print('Creating directory "Change Packets"')
+os.mkdir('Change Packets')
+print('Created directory "Change Packets"')
+
+for match in matchesJSON:
+	matchName = match['comp_level'][:1].upper() + str(match['match_number'])
+	for alliance in match['alliances']:
+		for team in match['alliances'][alliance]['teams']:
+			teamNum = int(team[3:])
+
+			j = {}
+			j['class'] = 'Team'
+			j['uniqueValue'] = teamNum
+			j['allianceColor'] = alliance
+
+			cs = []
+			for key in keys:
+				c = {}
+				c['keyToChange'] = 'matchData.' + matchName + '.' + key
+				c['valueToChangeTo'] = -1
+				cs.append(c)
+
+			ct = {}
+			ct['keyToChange'] = 'name'
+			ct['valueToChangeTo'] = teamNames[teamNum]
+			cs.append(ct)
+			j['changes'] = cs
+
+			fileName = 'Change Packets/' + str(teamNum) + '_' + matchName + '_creation.json'
+			f = open(fileName, 'w')
+			print('Writing to file ' + fileName)
+			print(json.dumps(j, sort_keys=True, indent=4, separators=(',', ': ')), file=f)
